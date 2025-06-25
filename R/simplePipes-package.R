cfc502868607d021938b811bbb8332e27225dd4d
Craig Wendorf <cwendorf@uwsp.edu>
2025-06-25 13:53:30 -0500

    Updated documentation

---
+++ R/simplePipes-package.R
@@ -0,0 +1,18 @@
+#' simplePipes: A Collection Pipe Operators
+#'
+#' simplePipes provides a collection of lightweight pipe operators for R.
+#'
+#' @details
+#' simplePipes is a small R package that implements a variety of simple pipe operators. 
+#' It has less error checking than major R packages (like magrittr or dplyr) but contains more varieties -- 
+#' both forward and backward versions of basic, tee, wye, exposition, compound assignment, and dot pipes -- 
+#' and introduces the concept of through pipes. It also implements syntax that is simpler than the R native forward pipe.
+#'
+#' @author
+#' **Maintainer**: Craig Wendorf  
+#' \email{cwendorf@uwsp.edu}
+#'
+#' @docType package
+#' @name simplePipes
+#' @aliases simplePipes-package
+"_PACKAGE"
